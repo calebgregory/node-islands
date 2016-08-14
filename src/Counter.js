7350b0930894d48e90ec07b0bb1d1f33daf2d8d5
calebgregory <calebgregory@gmail.com>
2016-08-13 22:37:21 -0500

    Adding toConnections, connect, connectMap to Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,49 +1,35 @@
+const _ = require('ramda')
+
+const R = (map) => (i, j) => map[i][j+1] || 0
+const D = (map) => (i, j) => (map[i+1] || {})[j] || 0
+
+const toConnections = (map, i) => (knx, e, j) => {
+  if (e) {
+    if (R(map)(i,j)) knx = _.assocPath([i, j, i  ], j+1)(knx)
+    if (D(map)(i,j)) knx = _.assocPath([i, j, i+1], j  )(knx)
+  }
+  return knx
+}
+
+const connect = (map) => (row, i) => row.reduce(toConnections(map, i), {})
+
+const connectMap = (map) => map.reduce((acc, r, i) => _.merge(acc)(connect(map)(r, i)), {})
+
 const numberOfIslands = (map) => {
-
-  const L = (i, j) => map[i][j-1] || 0
-  const R = (i, j) => map[i][j+1] || 0
-  const U = (i, j) => (map[i-1] || {})[j] || 0
-  const D = (i, j) => (map[i+1] || {})[j] || 0
-
-  let coordinates = []
-  let count = 0
 
   console.log()
   map.forEach( row => console.log(row) )
   console.log()
 
-  console.log('e (i, j) | {R} {D}')
-  console.log()
+  const connections = connectMap(map)
 
-  map.forEach((row, i) => {
-    row.forEach((e, j) => {
-      const coords = `${i},${j}`
+  console.log(JSON.stringify(connections, null, 2))
 
-      console.log(`${e} (${i}, ${j}) | >${R(i,j)}> v${D(i,j)}v`)
-
-      if (e) {
-        const indexOfCoords = coordinates.indexOf(coords)
-        if (indexOfCoords < 0) {
-          count++
-          console.log(count)
-          if (R(i,j)) { coordinates.push(`${i},${j+1}`); console.log(`   ${i}, ${j+1}`) }
-          if (D(i,j)) { coordinates.push(`${i+1},${j}`); console.log(`   ${i+1}, ${j}`) }
-        } else {
-          console.log('N:', count, 'coordinates:', coordinates)
-          coordinates = [
-            ...coordinates.slice(0, indexOfCoords),
-            ...coordinates.slice(indexOfCoords+1)
-          ]
-        }
-      }
-
-    })
-      console.log()
-  })
-
-  return count
+  return;
 }
 
 module.exports = {
+  toConnections,
+  connect,
   numberOfIslands
 }
--- test/Counter.spec.js
+++ test/Counter.spec.js
@@ -3,7 +3,7 @@ const { expect } = require('chai')
 const Counter = require('../src/Counter')
 
 describe('Counter', () => {
-  describe('numIslands', () => {
+  describe.only('numIslands', () => {
     it("counts two islands if there are two 1's not touching each other", () => {
       const map = [
         [0, 0, 1],
@@ -25,6 +25,41 @@ describe('Counter', () => {
       const numberOfIslands = Counter.numberOfIslands(map)
 
       expect(numberOfIslands).to.equal(1)
+    })
+  })
+
+  describe('connect', () => {
+    it('takes map and row and index, creates an entry in tree for Downward connection', () => {
+      const map = [
+        [0, 0, 1],
+        [1, 1, 1],
+        [1, 0, 0]
+      ]
+
+      const connect = Counter.connect(map)(map[0], 0)
+
+      const expected = { 0: { 2: { 1: 2 } } }
+
+      expect(connect).to.eql(expected)
+    })
+
+    it('takes map and row and index, creates an entry in tree for Rightward connection', () => {
+      const map = [
+        [0, 0, 1],
+        [1, 1, 1],
+        [1, 0, 0]
+      ]
+
+      const connect = Counter.connect(map)(map[1], 1)
+
+      const expected = {
+        1: {
+          0: { 1: 1, 2: 0 },
+          1: { 1: 2 }
+        }
+      }
+
+      expect(connect).to.eql(expected)
     })
   })
 })
