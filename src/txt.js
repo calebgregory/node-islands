2f3b424a6a0118edd92250ef5b8260ad815bafbc
calebgregory <calebgregory@gmail.com>
2016-08-14 19:40:06 -0500

    Drawing @ instead of 1

--- src/txt.js
+++ src/txt.js
@@ -6,7 +6,7 @@ it randomly draws an x * y map and counts the islands of 1s in the sea of 0s
 please enter the size of each dimension x and y...
 `
 
-const drawTextMap = (map) => map.reduce((acc, row) => `${acc}${row.join(' ').replace(/0/g, ' ')}\n`, '')
+const drawTextMap = (map) => map.reduce((acc, row) => `${acc}${row.join(' ').replace(/0/g, ' ').replace(/1/g, '@')}\n`, '')
 
 const caption = (x, y, cnt) => `
 a ${x} * ${y} map with ${cnt} islands
